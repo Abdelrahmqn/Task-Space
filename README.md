d4236af6abe006c8fe25aa6371a2df75ab13e309
abdelrahman <boda8099@gmail.com>
2024-06-27 04:29:50 +0300

    user log in page, users, database

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Task-Space
 Your creative Tasks and Notes in one place!
+<img src="/Task-Space/static/my-main.jpg">
--- app.py
+++ app.py
@@ -1,8 +1,75 @@
-# !/usr/bin/python3
-from website import application
+#!/usr/bin/python3
+""" """
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import path
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
-app = application()
+db = SQLAlchemy()
+NAME = "datab.db"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(30)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{NAME}'
+db.init_app(app)
+
+
+def create_database():
+    """ """
+    if not path.exists('website/' + NAME):
+        with app.app_context():
+            db.create_all()
+            print("created")
+
+
+class User(db.Model, UserMixin):
+    first_name = db.Column(db.String(200))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
+    notes = db.relationship('Notes')
+
+
+class Notes(db.Model):
+    text = db.Column(db.String(20000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class ToDoList():
+    """ """
+    pass
+
+@app.route('/')
+@app.route('/Home')
+@app.route('/home')
+def home_page():
+    """ """
+    return render_template("home.html")
+
+
+@app.route('/login')
+def log_in():
+    """ """
+    return render_template("login.html")
+
+
+@app.route('/sign-up')
+def sign_up():
+    """ """
+    return render_template("website/templates/login.html")
+
+
+@app.route('/logout')
+def logout():
+    """ """
+    return render_template("logout.html")
+
 
 if __name__ == "__main__":
+    create_database()
     app.run(debug=True)
