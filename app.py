2616c53a8f778b4129a6646352cece34c2a11d07
abdelrahman <boda8099@gmail.com>
2024-06-28 00:38:02 +0300

    error log in

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from os import path
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from sqlalchemy.sql import func
-
 
 
 db = SQLAlchemy()
@@ -20,7 +19,7 @@ db.init_app(app)
 
 def create_database():
     """ """
-    if not path.exists('website/' + NAME):
+    if not path.exists(NAME):
         with app.app_context():
             db.create_all()
             print("created")
@@ -46,7 +45,6 @@ class ToDoList():
     pass
 
 @app.route('/')
-@app.route('/Home')
 @app.route('/home')
 def home_page():
     """ """
@@ -58,41 +56,52 @@ def log_in():
     """ """
     if request.method == 'POST':
         email = request.form.get('email')
-        password = request.form.get('passwrod')
-        user = User.query.filter_by(email=email)
-        if user:
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            login_user(user)
             flash('logged in!', category='success')
+            return redirect(url_for('home_page'))
         else:
             flash('login error, try again.', category='error')
-    else:
-        flash('Email error, try again.', category='error')
     return render_template("login.html", boolean=True)
 
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     """ """
-    email = request.form.get('gmail')
-    first_name = request.form.get('first_name')
-    password = request.form.get('password')
-    password2 = request.form.get('password2')
-    # if len(email) < 5:
-    #     flash('email must be greater than 5 characters', category='error')
-    # elif len(first_name) < 2:
-    #     flash('your first name must be greater than 2 characters', category='error')
-    # elif password != password2:
-    #     flash('Your passwords don\'t match', category='error')
-    # elif len(password) < 8:
-    #     flash('Your password must be greater than 8 characters', category='error')
-    # else:
-    #     flash('Account Created!', category='success')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        
+        if not first_name:
+            flash('First name is required', category='error')
+        elif len(first_name) < 2:
+            flash('Your first name must be greater than 2 characters', category='error')
+        elif not password:
+            flash('Password is required', category='error')
+        elif password != password2:
+            flash('Your passwords don\'t match', category='error')
+        elif len(password) < 8:
+            flash('Your password must be greater than 8 characters', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account Created!', category='success')
+            return redirect(url_for('home_page'))
     return render_template("sign_up.html")
 
 
 @app.route('/logout')
+@login_required
 def logout():
     """ """
-    return render_template("logout.html")
+    logout_user()
+    flash('You have been logged out.', category='success')
+    return redirect(url_for('home_page'))
 
 
 if __name__ == "__main__":
